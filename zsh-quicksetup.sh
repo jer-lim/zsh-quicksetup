4f577ef939441d96590e48e8e8f24de692048fe3
Jeremy Lim <me@jerl.im>
2020-05-24 00:22:35 +0800

    Update zsh-quicksetup.sh

--- zsh-quicksetup.sh
+++ zsh-quicksetup.sh
@@ -6,4 +6,4 @@ sudo apt install zsh -y
 sudo chsh -s `which zsh` $USER
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
 wget https://raw.githubusercontent.com/jer-lim/zsh-quicksetup/master/zsh-quicksetup-2.sh -O - | zsh
-zsh
+exec zsh
