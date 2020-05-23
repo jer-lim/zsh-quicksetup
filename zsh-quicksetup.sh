55c3c3c55fea83981985f50b41903d6544fb1933
Jeremy Lim <github@jerl.im>
2020-05-23 12:09:29 -0400

    Add files via upload

---
+++ zsh-quicksetup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+sudo apt update
+sudo apt upgrade -y
+sudo apt install zsh -y
+sudo chsh -s `which zsh` $USER
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+wget https://raw.githubusercontent.com/jer-lim/zsh-quicksetup/master/zsh-quicksetup-2.sh -O - | zsh
+zsh
