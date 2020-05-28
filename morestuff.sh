a9e17fec4db49de8f90058e164f867a2d9629c8c
Jeremy Lim <me@jerl.im>
2020-05-29 00:53:18 +0800

    Create morestuff.sh

---
+++ morestuff.sh
@@ -0,0 +1,20 @@
+#!/bin/zsh
+
+# stuff
+sudo apt install python3 python3-pip glances -y
+
+# docker https://docs.docker.com/engine/install/ubuntu/
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io -y
+sudo usermod -aG docker $USER
