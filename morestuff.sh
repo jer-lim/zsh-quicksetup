8a8a98ffcc3a87fb83b30da4946a29bb21bed97c
Jeremy Lim <me@jerl.im>
2020-05-29 01:00:16 +0800

    Update morestuff.sh

--- morestuff.sh
+++ morestuff.sh
@@ -4,12 +4,12 @@
 sudo apt install python3 python3-pip glances -y
 
 # docker https://docs.docker.com/engine/install/ubuntu/
-sudo apt-get install \
+sudo apt install \
     apt-transport-https \
     ca-certificates \
     curl \
     gnupg-agent \
-    software-properties-common
+    software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
